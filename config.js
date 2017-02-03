040f54483ed2afc868395930a4d60556c00b6c33
Ivan <zanusilker@gmail.com>
2017-02-03 06:50:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let project = {
   name: "gulpimize", // You need to input name your project
-  pablicPath: "https://zanusilker.github.io/gulpimize", // You need to input path your project without / on end. Example "http://ivanchaban.hol.es/gulpimize" 
-  minifyHTML: false // Do you want compress html?
+  pablicPath: "https://zanusilker.github.io/gulpimize", // You need to input path your project without '/' on end. Example "http://ivanchaban.hol.es/gulpimize" 
+  minifyHTML: false // Do you want to compress html?
 };
 
 module.exports = project;
