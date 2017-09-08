935ef0520706a813d0126e1a2b440ec2a9517a4d
Ivan Chaban <zanusilker@gmail.com>
2017-09-08 14:58:55 +0300

    Gulpimize 6 Site

--- build/js/0.js
+++ build/js/0.js
@@ -1,1 +1,14 @@
-webpackJsonp_name_([0],{6:function(a,n,c){"use strict";alert("Hi, I am from dynamic.js")}});
+webpackJsonp_name_([0],{
+
+/***/ 6:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+alert("Hi, I am from dynamic.js");
+
+/***/ })
+
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9zcmMvanMvbW9kdWxlcy9keW5hbWljLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxyXG5hbGVydChcIkhpLCBJIGFtIGZyb20gZHluYW1pYy5qc1wiKTtcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gc3JjL2pzL21vZHVsZXMvZHluYW1pYy5qcyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFDQTs7OztBIiwic291cmNlUm9vdCI6IiJ9
