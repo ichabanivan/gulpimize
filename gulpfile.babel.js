d5847ae0fa3da43be420fdf406418cc493d7929f
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-02-22 17:33:12 +0200

    Changed gulpfile.js on es6

---
+++ gulpfile.babel.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import gulp from 'gulp';
+import fileInclude from 'gulp-file-include';
+
+const dirs = {
+	src: 'src',
+	dest: 'build'
+ };
+
+ const path = {
+	src: `${dirs.src}*.html`,
+	dest: `${dirs.dest}`
+ };
+
+gulp.task('fileInclude', () => {
+  return gulp.src(path.src)
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(path.dest));
+});
