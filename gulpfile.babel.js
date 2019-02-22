6d08563ecf1fdaee42e815af919c782badad8166
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-02-23 00:01:13 +0200

    Added sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,38 @@
 
 import gulp from 'gulp';
 import fileInclude from 'gulp-file-include';
+import sass from 'gulp-sass';
 
 const dirs = {
 	src: 'src',
 	dest: 'build'
  };
 
- const path = {
+const htmlPath = {
 	src: `${dirs.src}/*.html`,
 	dest: `${dirs.dest}/`
- };
+};
+
+const scssPath = {
+	src: `${dirs.src}/scss/**/*.scss`,
+	dest: `${dirs.dest}/css`
+};
 
 gulp.task('fileInclude', () => {
-  return gulp.src(path.src)
+  return gulp.src(htmlPath.src)
     .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest(path.dest));
+    .pipe(gulp.dest(htmlPath.dest));
+});
+ 
+gulp.task('scss', () => {
+  return gulp.src(scssPath.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest((scssPath.dest)));
+});
+ 
+gulp.task('sass:watch', () => {
+  gulp.watch(scssPath.src, ['sass']);
 });
