016cd63c81f73eb1e0a574087be10aae4576a4b1
Ivan Chaban <zanusilker@gmail.com>
2019-02-24 21:47:59 +0200

    Todo tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// TODO: Please, Remove line above
 import gulp from 'gulp';
 import fileInclude from 'gulp-file-include';
 import sass from 'gulp-sass';
@@ -10,6 +10,19 @@ import autoprefixer from 'gulp-autoprefixer';
 import del from 'del';
 import imagemin from 'gulp-imagemin';
 
+// TODO: Please use gulp-plumber
+/*
+.pipe(plumber({
+	errorHandler: notify.onError((err) => {
+		return {
+			title: 'sass',
+			message: err.message
+		}
+	})
+}))
+*/
+
+// TODO: Please make 1 object with nested properties
 const dirs = {
 	src: 'src',
 	dest: 'build'
@@ -39,17 +52,20 @@ gulp.task('fileInclude', () => {
     }))
     .pipe(gulp.dest(htmlPath.dest));
 });
- 
+
+// TODO: Please add postcss after sass
+// TODO: Please use autoprefixer like postcss plugin
+
 gulp.task('style', () => {
   return gulp.src(scssPath.src)
-		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sourcemaps.init({loadMaps: true})) // TODO: Please add environments and use this line only if it is development
 			.pipe(plumber())
-			.pipe(sass().on('error', sass.logError))
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
+			.pipe(sass().on('error', sass.logError)) // Todo: Change to gulp plumber, you've forgotten about settings for sass outputStyle: 'expanded'
+			.pipe(autoprefixer({ // please use
+				browsers: ['last 2 versions'], // ['last 10 versions', '> 0.5%']
 				cascade: false
 			}))
-			.pipe(csso({
+			.pipe(csso({ // TODO: Please add environments and use this line only if it is production
 				restructure: false,
 				sourceMap: true,
 				debug: true
@@ -59,6 +75,8 @@ gulp.task('style', () => {
     .pipe(gulp.dest((scssPath.dest)));
 });
 
+// TODO: Please, read about gulp-newer
+// TODO: You forgot about browserSync
 gulp.task('imageMin', () =>
 	gulp.src(imgPath.src)
 	.pipe(imagemin([
@@ -75,6 +93,11 @@ gulp.task('imageMin', () =>
   .pipe(gulp.dest(imgPath.dest))
 );
 
+/* TODO:
+  return del(PATH.build.folder, {
+    force: true
+  })
+*/
 gulp.task('clean', () => {
 	return del(['build']);
 });
