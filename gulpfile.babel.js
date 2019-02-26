e63d5a1b27c7c4ac824960b91111f2716cc6de15
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-02-26 21:38:45 +0200

    Starter 0.1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,22 @@
-'use strict';
-// TODO: Please, Remove line above
 import gulp from 'gulp';
 import fileInclude from 'gulp-file-include';
 import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
 import csso from 'gulp-csso';
 import plumber from 'gulp-plumber';
-import autoprefixer from 'gulp-autoprefixer';
+import notify from 'gulp-notify';
 import del from 'del';
 import imagemin from 'gulp-imagemin';
+
+import cssNext from 'postcss-cssnext'; // This plugin can replace autoprefixer. It makes sense? yesss
+import doIUse from 'doiuse';
+import flexBugs from 'postcss-flexbugs-fixes';
+
+import newer from 'gulp-newer';
+import watch from 'gulp-watch'; 
+import {create as bsCreate} from 'browser-sync';
+const browserSync = bsCreate();
 
 // TODO: Please use gulp-plumber
 /*
@@ -23,48 +31,83 @@ import imagemin from 'gulp-imagemin';
 */
 
 // TODO: Please make 1 object with nested properties
-const dirs = {
-	src: 'src',
-	dest: 'build'
- };
-
-const htmlPath = {
-	src: `${dirs.src}/*.html`,
-	dest: `${dirs.dest}/`
-};
-
-const scssPath = {
-	src: `${dirs.src}/scss/**/*.scss`,
-	dest: `${dirs.dest}/css`
-};
-
-const imgPath = {
-	src: `${dirs.src}/assets/img/**/*`,
-	dest: `${dirs.dest}/img`
-};
-
+const PATH = {
+	src: {
+		html: './src/*.html',
+		scss: './src/scss/**/*.scss',
+		img: './src/assets/img/**/*'
+	},
+	dirs: {
+		html: './build/',
+		scss: './build/css',
+		img: './build/img'
+	},
+	build: {
+		folder: './build'
+	}
+}
 
 gulp.task('fileInclude', () => {
-  return gulp.src(htmlPath.src)
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest(htmlPath.dest));
+	return gulp.src(PATH.src.html, {since: gulp.lastRun('fileInclude')})
+		.pipe(plumber({
+			errorHandler: notify.onError((err) => {
+				return {
+					title: 'html',
+					message: err.message
+				}
+			})
+		}))
+		.pipe(newer(PATH.dirs.html))
+		.pipe(fileInclude({
+			prefix: '@@',
+				basepath: '@file'
+			}))
+		.pipe(gulp.dest(PATH.dirs.html));
 });
 
 // TODO: Please add postcss after sass
 // TODO: Please use autoprefixer like postcss plugin
 
+
+
 gulp.task('style', () => {
-  return gulp.src(scssPath.src)
+
+	let postcssPlugins = [
+		cssNext ({
+			browsers: ['last 10 versions', '> 0.5%'],
+			cascade: false
+		}),
+		doIUse ({
+			browsers: [
+				'ie >= 8',
+				'> 1%'
+			],
+			ignore: ['rem'], // an optional array of features to ignore
+			ignoreFiles: ['**/normalize.css'], // an optional array of file globs to match against original source file path, to ignore
+			onFeatureUsage: function (usageInfo) {
+				console.log(usageInfo.message)
+			}
+		}),
+		flexBugs ({
+			bug6: false 
+		})
+	]
+
+	return gulp.src(PATH.src.scss, {since: gulp.lastRun('style')})
+		.pipe(plumber({
+			errorHandler: notify.onError((err) => {
+				return {
+					title: 'style',
+					message: err.message
+				}
+			})
+		}))
 		.pipe(sourcemaps.init({loadMaps: true})) // TODO: Please add environments and use this line only if it is development
-			.pipe(plumber())
-			.pipe(sass().on('error', sass.logError)) // Todo: Change to gulp plumber, you've forgotten about settings for sass outputStyle: 'expanded'
-			.pipe(autoprefixer({ // please use
-				browsers: ['last 2 versions'], // ['last 10 versions', '> 0.5%']
-				cascade: false
-			}))
+			.pipe(newer(PATH.dirs.scss))
+			.pipe(sass({
+				outputStyle: 'expanded'
+			})) // Todo: Change to gulp plumber, you've forgotten about settings for sass outputStyle: 'expanded'
+			.pipe(postcss(postcssPlugins))
 			.pipe(csso({ // TODO: Please add environments and use this line only if it is production
 				restructure: false,
 				sourceMap: true,
@@ -72,26 +115,35 @@ gulp.task('style', () => {
 			}))
 			.pipe(plumber.stop())
 		.pipe(sourcemaps.write())
-    .pipe(gulp.dest((scssPath.dest)));
+    .pipe(gulp.dest((PATH.dirs.scss)));
 });
 
 // TODO: Please, read about gulp-newer
 // TODO: You forgot about browserSync
-gulp.task('imageMin', () =>
-	gulp.src(imgPath.src)
-	.pipe(imagemin([
-		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 5}),
-		imagemin.svgo({
-			plugins: [
-					{removeViewBox: true},
-					{cleanupIDs: false}
-			]
-		})
-	]))
-	.pipe(gulp.dest(imgPath.dest))
-);
+gulp.task('imageMin', () => {
+	return gulp.src(PATH.src.img, {since: gulp.lastRun('imageMin')})
+		.pipe(plumber({
+			errorHandler: notify.onError((err) => {
+				return {
+					title: 'img',
+					message: err.message
+				}
+			})
+		}))
+		.pipe(newer(PATH.dirs.img))
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+						{removeViewBox: true},
+						{cleanupIDs: false}
+				]
+			})
+		]))
+		.pipe(gulp.dest(PATH.dirs.img))
+});
 
 /* TODO:
   return del(PATH.build.folder, {
@@ -99,13 +151,30 @@ gulp.task('imageMin', () =>
   })
 */
 gulp.task('clean', () => {
-	return del(['build']);
+	return del(PATH.build.folder, {
+		force: true
+  })
 });
 
-gulp.task('default', gulp.series('clean', gulp.parallel('fileInclude', 'style'), 'imageMin'));
+gulp.task('build', gulp.series('clean', gulp.parallel('fileInclude', 'style'), 'imageMin'));
 
-// what is it for
+gulp.task('watch', () => {
+	gulp.watch(PATH.src.html, gulp.series('fileInclude'));
+	gulp.watch(PATH.src.scss, gulp.series('style'));
+	gulp.watch(PATH.src.img, gulp.series('imageMin'));
+});
 
-// gulp.task('sass:watch', () => {
-//   gulp.watch(scssPath.src, ['sass']);
-// });
+gulp.task('serve', () => {
+	browserSync.init({
+		server: {
+			baseDir: PATH.build.folder
+		},
+    notify: false
+	});
+	
+	// browserSync.watch('build/**/*.*').on('change', browserSync.reload);
+})
+
+gulp.task('default', 
+	gulp.series('build', gulp.parallel('watch', 'serve'))
+);
