766ba92ede897fb601fa4b96f571b6555ccefd2e
Ivan Chaban <zanusilker@gmail.com>
2019-03-05 14:51:29 +0200

    Clear

--- gulpimize-config.js
+++
@@ -1,5 +0,0 @@
-let project = {
-  publicPath: 'https://zanusilker.github.io/gulpimize/build' // You need to input path your project without / on end. Example "https://zanusilker.github.io/gulpimize/build"
-};
-
-module.exports = project;
