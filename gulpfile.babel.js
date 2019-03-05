5539a6b2e21522e222ca9854a33f8e52ea542879
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-03-05 22:23:13 +0200

    Fixed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,7 +20,7 @@ import newer from 'gulp-newer';
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
-// import watch from 'gulp-watch';
+import watch from 'gulp-watch';
 import { create } from 'browser-sync';
 
 const browserSync = create();
@@ -45,18 +45,20 @@ const PATH = {
   }
 };
 
-gulp.task('fileInclude', () => gulp.src(PATH.src.html)
-  .pipe(plumber({
-    errorHandler: notify.onError(err => ({
-      title: 'html',
-      message: err.message
+gulp.task('fileInclude', () => {
+  return gulp.src(PATH.src.html, {since: gulp.lastRun('fileInclude')})
+    .pipe(plumber({
+      errorHandler: notify.onError(err => ({
+        title: 'html',
+        message: err.message
+      }))
     }))
-  }))
-  .pipe(fileInclude({
-    prefix: '@@',
-    basepath: '@file'
-  }))
-  .pipe(gulp.dest(PATH.dirs.html)));
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(PATH.dirs.html))
+});
 
 gulp.task('style', () => {
   const postcssPlugins = [
@@ -80,7 +82,7 @@ gulp.task('style', () => {
     })
   ];
 
-  return gulp.src(PATH.src.scss)
+  return gulp.src(PATH.src.scss, {since: gulp.lastRun('style')})
     .pipe(plumber({
       errorHandler: notify.onError(err => ({
         title: 'style',
@@ -100,63 +102,72 @@ gulp.task('style', () => {
     })))
     .pipe(plumber.stop())
     .pipe(development(sourcemaps.write()))
-    .pipe(gulp.dest((PATH.dirs.scss)));
+    .pipe(gulp.dest((PATH.dirs.scss)))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('scripts', () => gulp.src(PATH.src.js)
-  .pipe(plumber({
-    errorHandler: notify.onError(err => ({
-      title: 'scripts',
-      message: err.message
+gulp.task('scripts', () => {
+  return gulp.src(PATH.src.js, {since: gulp.lastRun('scripts')})
+    .pipe(plumber({
+      errorHandler: notify.onError(err => ({
+        title: 'scripts',
+        message: err.message
+      }))
     }))
-  }))
-  .pipe(development(sourcemaps.init({ loadMaps: true })))
-  .pipe(babel({
-    presets: ['@babel/env']
-  }))
-  .pipe(concat('all.js'))
-  .pipe(production(uglify()))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(plumber.stop())
-  .pipe(development(sourcemaps.write()))
-  .pipe(gulp.dest((PATH.dirs.js)))
-  .pipe(browserSync.reload({ stream: true })));
+    .pipe(development(sourcemaps.init({ loadMaps: true })))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('all.js'))
+    .pipe(production(uglify()))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(plumber.stop())
+    .pipe(development(sourcemaps.write()))
+    .pipe(gulp.dest((PATH.dirs.js)))
+    .pipe(browserSync.reload({ stream: true }))
+});
 
-gulp.task('imageMin', () => gulp.src(PATH.src.img)
-  .pipe(plumber({
-    errorHandler: notify.onError(err => ({
-      title: 'img',
-      message: err.message
+gulp.task('imageMin', () => {
+  return gulp.src(PATH.src.img, {since: gulp.lastRun('imageMin')})
+    .pipe(plumber({
+      errorHandler: notify.onError(err => ({
+        title: 'img',
+        message: err.message
+      }))
     }))
-  }))
-  .pipe(newer(PATH.dirs.img))
-  .pipe(imagemin([
-    imagemin.gifsicle({ interlaced: true }),
-    imagemin.jpegtran({ progressive: true }),
-    imagemin.optipng({ optimizationLevel: 5 }),
-    imagemin.svgo({
-      plugins: [
-        { removeViewBox: true },
-        { cleanupIDs: false }
-      ]
-    })
-  ]))
-  .pipe(gulp.dest(PATH.dirs.img)));
+    .pipe(newer(PATH.dirs.img))
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest(PATH.dirs.img))
+});
 
 gulp.task('clean', () => del(PATH.build.folder, {
   force: true
 }));
 
-gulp.task('eslint', () => gulp.src(['**/*.js', '!node_modules/**'])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
-  .pipe(eslint.formatEach('compact', process.stderr)));
+gulp.task('eslint', () => {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(eslint.formatEach('compact', process.stderr))
+});
 
-gulp.task('eslintFix', () => gulp.src(['**/*.js', '!node_modules/**'])
-  .pipe(eslint({ fix: true }))
-  .pipe(eslint.format())
-  .pipe(gulp.dest('./')));
+gulp.task('eslintFix', () => {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint({ fix: true }))
+    .pipe(eslint.format())
+    .pipe(gulp.dest('./'))
+});
 
 gulp.task('build', gulp.series('clean', gulp.parallel('fileInclude', 'style', 'scripts'), 'imageMin'));
 
